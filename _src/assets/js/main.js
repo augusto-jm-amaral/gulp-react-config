c1f5eca6075d1011b304b366a1f30a6f75e9575d
Augusto Amaral <augusto.jm.amaral1@gmail.com>
2017-03-03 18:32:04 -0300

    State

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,35 +1,63 @@
-var Nav = React.createClass({
-	render: function () {
-		return (
-			<nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-			  <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			    <span className="navbar-toggler-icon"></span>
-			  </button>
-			  <a className="navbar-brand" href="#">Navbar</a>
+// var Nav = React.createClass({
+// 	render: function () {
+// 		return (
+// 			<nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+// 			  <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+// 			    <span className="navbar-toggler-icon"></span>
+// 			  </button>
+// 			  <a className="navbar-brand" href="#">Navbar</a>
 
-			  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-			    <ul className="navbar-nav mr-auto">
-			      <li className="nav-item active">
-			        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-			      </li>
-			      <li className="nav-item">
-			        <a className="nav-link" href="#">Link</a>
-			      </li>
-			      <li className="nav-item">
-			        <a className="nav-link disabled" href="#">Disabled</a>
-			      </li>
-			    </ul>
-			    <form className="form-inline my-2 my-lg-0">
-			      <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
-			      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-			    </form>
-			  </div>
-			</nav>
-		);
+// 			  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+// 			    <ul className="navbar-nav mr-auto">
+// 			      <li className="nav-item active">
+// 			        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+// 			      </li>
+// 			      <li className="nav-item">
+// 			        <a className="nav-link" href="#">Link</a>
+// 			      </li>
+// 			      <li className="nav-item">
+// 			        <a className="nav-link disabled" href="#">Disabled</a>
+// 			      </li>
+// 			    </ul>
+// 			    <form className="form-inline my-2 my-lg-0">
+// 			      <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
+// 			      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+// 			    </form>
+// 			  </div>
+// 			</nav>
+// 		);
+// 	}
+// });
+
+var Button = React.createClass({
+	
+	getInitialState : function () {
+		return {
+			click: false
+		};
+	},
+
+	toggleClick: function () {
+		this.setState({
+			click: !this.state.click 
+		});
+	},
+
+	render : function () {
+		var btnClass = this.state.click ? 'btn btn-primary' : 'btn btn-default';
+
+		return (
+			<button onClick={ this.toggleClick } className={ btnClass }>{ this.state.click ? this.props.textActive : this.props.title  }</button>
+			);
 	}
-});
+})
+
+// ReactDOM.render(
+// 		<Nav title="React" />,
+// 		document.getElementById('app')
+// 	);
 
 ReactDOM.render(
-		<Nav title="React" />,
-		document.getElementById('app')
+		<Button title="Meu Botão" textActive="Loading..."/>,
+		document.getElementById('myButton')
 	);
